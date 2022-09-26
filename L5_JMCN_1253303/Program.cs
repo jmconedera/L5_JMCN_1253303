6526314f2b3f6512797ce18217d452db1764cd46
MCONEDERA <MCONEDERA@GLC2539>
2022-09-26 14:07:54 -0600

    Add project files.

---
+++ L5_JMCN_1253303/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numeroEntero =0;
+/*Mensaje en Pantalla*/
+Console.WriteLine("Ejercicio 1");
+Console.WriteLine("Numero Entero");
+/*Siguiente Instrucción recibe y almacena el numero*/
+numeroEntero = Convert.ToInt32(Console.ReadLine());
+if(numeroEntero<0)
+{
+  Console.WriteLine("Número Negativo");
+}else if(numeroEntero>1)
+{ Console.WriteLine("Número Positivo");
+}
